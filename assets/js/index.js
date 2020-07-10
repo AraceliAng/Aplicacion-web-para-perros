42e2b54437546530740c59702c89eb2bd95e3bdd
AraceliAng <olguinaraceli@gmail.com>
2020-07-09 22:53:29 -0500

    agregando chat

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,26 @@
-
-
+const firebaseConfig = {
+    apiKey: "AIzaSyDDpo5H8nSwV5ooYC6sRS2e7PXKSqNqUQQ",
+    authDomain: "chatpatasenmarcha.firebaseapp.com",
+    databaseURL: "https://chatpatasenmarcha.firebaseio.com",
+    projectId: "chatpatasenmarcha",
+    storageBucket: "chatpatasenmarcha.appspot.com",
+    messagingSenderId: "937755336222",
+    appId: "1:937755336222:web:f3be9414845ca90e4e580f"
+};
 firebase.initializeApp(firebaseConfig);
-
-// const messageText = document.getElementById("message");
-// const chatArea = document.getElementById('chat');
- const button = document.getElementById('btnSend');
-
-
 const db = firebase.database();  
 
 $(document).ready(function(){
     db.ref('messageChat').on('child_added', function(data){
-        $('#chat').append(data.val().mensaje + '<br>');
-        UIkit.scroll(data).scrollTop(data);
+        $('#chat').append(data.val().message + '<br>');
+        //UIkit.scroll(data).scrollTop(data);
     });
 });
  $('#btnSend').on('click', function(){
      const message = $('#message').val();  
      
      db.ref('messageChat').push({
-         mensaje: message
+         message: message
      });
      $('#message').val('');
      //$("#chat").scrollTop($('#chat').prop('scrollHeight'));
@@ -38,15 +39,28 @@ $(document).ready(function(){
 
 function validate(){
     const validateInput = true;
-    element = document.getElementsByClassName("inputText");
+    const element = document.getElementsByClassName("inputText");
     for (i = 0; i< element.length; i++){
         if (element[i].value === "" || element[i].value == null){
             validateInput = false
         }
     }
     if(validateInput){
+        
         document.getElementById("btnSend").disabled = false;
     }else{
+        
         document.getElementById("btnSend").disabled = true;
     }
 }
+
+
+function hideChat(){
+    const chatContainer = document.getElementById("chatContainer");
+    if(chatContainer.style.display === "none"){
+        chatContainer.style.display = "block";
+    } else {
+        chatContainer.style.display = "none";
+    }
+}
+
