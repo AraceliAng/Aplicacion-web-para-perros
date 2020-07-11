119b9647ff70a0bbf9d324c17fe06c4104ddd327
AraceliAng <olguinaraceli@gmail.com>
2020-07-10 19:25:58 -0500

    modificando el chat de la pagina

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,11 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDDpo5H8nSwV5ooYC6sRS2e7PXKSqNqUQQ",
-    authDomain: "chatpatasenmarcha.firebaseapp.com",
-    databaseURL: "https://chatpatasenmarcha.firebaseio.com",
-    projectId: "chatpatasenmarcha",
-    storageBucket: "chatpatasenmarcha.appspot.com",
-    messagingSenderId: "937755336222",
-    appId: "1:937755336222:web:f3be9414845ca90e4e580f"
-};
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();  
 
 $(document).ready(function(){
     db.ref('messageChat').on('child_added', function(data){
         $('#chat').append(data.val().message + '<br>');
-        //UIkit.scroll(data).scrollTop(data);
+        
     });
 });
  $('#btnSend').on('click', function(){
@@ -22,19 +14,10 @@ $(document).ready(function(){
      db.ref('messageChat').push({
          message: message
      });
+     
      $('#message').val('');
-     //$("#chat").scrollTop($('#chat').prop('scrollHeight'));
+     
  });
-
-// function getMessages() {
-//     const div = $("#chat");
-//     div.scrollTop(div.prop('scrollHeight'));
-// }
-
-// $(function() {
-//     getMessages();
-// });
-//
 
 
 function validate(){
